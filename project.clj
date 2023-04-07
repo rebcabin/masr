14097cb371fe10f29744054f1745b9f85c7bbd6d
rebcabin <rebcabin@users.noreply.github.com>
2023-04-06 17:17:54 -0700

    start

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure              "1.11.1"]
+                 [org.clojure/core.async           "1.6.673"]
+                 [com.github.blasterai/clj-fstring "1.1.2"]
+                 #_[org.clojure/data.zip             "1.0.0"]
+                 ]
   :main ^:skip-aot masr.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/masr/core.clj
+++ src/masr/core.clj
@@ -1,6 +1,20 @@
 (ns masr.core
   (:gen-class))
 
+;; | Variable(symbol_table parent_symtab,
+;;            identifier name,
+;;            identifier* dependencies,
+;;            intent intent,
+;;            expr? symbolic_value,
+;;            expr? value,
+;;            storage_type storage,
+;;            ttype type,
+;;            abi abi,
+;;            access access,
+;;            presence presence,
+;;            bool value_attr)
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
